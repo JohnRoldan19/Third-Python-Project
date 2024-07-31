28fad6857afe73d6f9a8b93a871a4464b9cc105d
JohnRoldan19 <johnroldanbandaypalentinos123@gmail.com>
2024-07-31 23:21:47 +0800

    flask

---
+++ backend/app.py
@@ -0,0 +1,24 @@
+# def my_func(name):
+#     if name == "John":
+#         print("John has entered the building")
+#     else:
+#         print("it is not john but it is:", name)
+
+#     for i in range(5):
+#         print(i, "second have passed")
+
+# my_func("Jane")
+
+
+# from db import db
+
+# print("this is my app.py file")
+
+
+ 
+from flask import Flask
+
+import express from "express"
+
+
+
---
+++ backend/db.py
@@ -0,0 +1,8 @@
+# print("this is my db file")
+
+# db = 25
+
+# # print("my db size is 25 GB")
+
+# if __name == "__main__":
+#     print("this file is called directly")
